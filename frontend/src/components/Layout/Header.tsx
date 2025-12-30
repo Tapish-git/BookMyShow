cf93c561c263d6a39bf88888e3cf6988fefd38cd
Tapish Bagdi <tapishbagdi@gmail.com>
2025-12-30 07:01:20 +0530

    removed useNaviagate

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -18,7 +18,7 @@ import {
     ListItemButton,
     ListItemText,
 } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { 
     Search, 
     LocationOn, 
